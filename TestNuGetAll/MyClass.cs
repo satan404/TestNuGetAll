7f7a216d7240eb432bf0c80b538700f2301b758b
Steven.Huang <Steven.Huang@doublegreen.com>
2020-03-09 14:53:18 +0800

    Go

---
+++ TestNuGetAll/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Account
+{
+    public interface User
+    {
+        // Property declaration:
+        string Name
+        {
+            get;
+            set;
+        }
+
+        // Property declaration:
+        string Email
+        {
+            get;
+            set;
+        }
+    }
+}
